7e9e14d47aa4faad7b694b1c66d6b5093ea7b0c9
cbaumler <chrisbaumler@gmail.com>
2015-11-07 12:05:45 -0600

    Add initial Atmel Studio project files and skeleton main.c

---
+++ 75fDemo/main.c
@@ -0,0 +1,18 @@
+/*
+ * 75fDemo.c
+ *
+ * Created: 11/7/2015 11:41:30 AM
+ * Author : Chris
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
